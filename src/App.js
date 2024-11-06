8e3083614ace0e98ba01cbd5ab450fc1535d5aab
Lubna Hajhussein <hajhussein.lubna@gmail.com>
2024-11-06 20:32:16 +0300

    improvments

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
-import { Route, Routes, HashRouter } from "react-router-dom";
 import Home from "./pages/Home";
-import Swap from "./pages/Swap";
 
 const App = () => {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/swap" element={<Swap />} />
-      </Routes>
-    </HashRouter>
+    <div>
+      <Home />
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
 import theme from "./utils/theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Router>
         <CssBaseline />
         <App />
-      </Router>
     </ThemeProvider>
   </React.StrictMode>
 );
